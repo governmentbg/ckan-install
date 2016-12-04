b183c0f94fdc948f8d88ad5d0334d516ba7e857f
Vasil Kolev <vasil@ludost.net>
2016-12-04 22:21:19 +0200

    backup script, initial import

---
+++ debian/dobackup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+. /root/ckan-install/config.sh
+
+dt=`date +%Y%m%d`
+
+dst=$BACKUP_DIR/$dt
+
+mkdir -p $dst
+
+cd $BACKUP_DIR
+
+for i in $BACKUP_PGDBNAMES; do
+	su postgres -c "pg_dump --blobs --format=c $i" > $dst/$i.pgsql.dump
+done
+
+for i in $BACKUP_PATHS; do
+	oname=`echo $i | sed 's%/%_%g'`
+	tar cf - $i 2>/dev/null | pbzip2 -c > $dst/$oname.tar.bz2
+done
+
+cd $BACKUP_DIR_TO_SYNC
+
+rsync -a * backup-opendata@koi.obshtestvo.bg:
+
