f5de23b51c9706d74ab8e8242fe2251df144a5fc
Dimitar Dimitrov <me@ddimitrov.name>
2016-12-04 23:21:50 +0200

    Add status messages to the backup script

--- debian/backup.sh
+++ debian/backup.sh
@@ -13,19 +13,22 @@ mkdir -p $dst
 cd $BACKUP_DIR
 
 for i in $BACKUP_PGDBNAMES; do
+	echo "Backing up PostgreSQL database $i"
 	su postgres -c "pg_dump --blobs --format=c $i" > $dst/$i.pgsql.dump
 done
 
 for i in $BACKUP_PATHS; do
+	echo "Backing up path $i"
 	oname=`echo $i | sed 's%/%_%g'`
 	tar cf - $i 2>/dev/null | nice pbzip2 -c > $dst/$oname.tar.bz2
 done
 
 if [ ! -z "$BACKUP_DAYS_TO_KEEP" ]; then
-	# Delete all backups older than $BACKUP_DAYS_TO_KEEP except
-	# backups made on the 1st of each month.
+	echo "Deleting backups older than $BACKUP_DAYS_TO_KEEP days (except backups made on the 1st of each month)..."
 	find $BACKUP_DIR -mindepth 1 -maxdepth 1 -type d -mtime +$BACKUP_DAYS_TO_KEEP | grep -v '01$' | xargs rm -rfv
 fi
+
+echo "Syncing all backups to $BACKUP_REMOTE_SYNC_LOCATION"
 
 cd $BACKUP_DIR_TO_SYNC
 
